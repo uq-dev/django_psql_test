c5c1ad8ddcc09efdce67ab89ba97f59ca9a7a47d
n_miyabe <hoge@hoge>
2017-06-24 18:59:30 +0900

    モデルの追加

--- psql_rest/map/admin.py
+++ psql_rest/map/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import japan_ver81
+
+@admin.register(japan_ver81)
+class UserAdmin(admin.ModelAdmin):
+	pass
--- psql_rest/map/models.py
+++ psql_rest/map/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class japan_ver81(models.Model):
+	gid = models.IntegerField(primary_key=True)
+	jcode = models.CharField(max_length=5)
+	ken = models.CharField(max_length=10)
+	sicho = models.CharField(max_length=20)
+	gun = models.CharField(max_length=20)
+	seirei = models.CharField(max_length=20)
+	sikuchoson = models.CharField(max_length=50)
+	city_eng = models.CharField(max_length=50)
+	p_num = models.IntegerField()
+	h_num = models.IntegerField()
+	geom = models.BinaryField()
+
+	class Meta:
+		db_table = 'japan_ver81'
